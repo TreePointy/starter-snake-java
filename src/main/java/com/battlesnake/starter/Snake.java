9f27748169184d3ff76ba275847dad946fd51d0f
Marcello La Posta <mlaposta@sasktel.net>
2020-11-16 11:15:03 -0600

    check for self-overlap

--- src/main/java/com/battlesnake/starter/Snake.java
+++ src/main/java/com/battlesnake/starter/Snake.java
@@ -186,36 +186,35 @@ public class Snake {
                 currentBodyPositions.add(new Coordinate(coordinate));
             });
 
-            if(head.getX() - 1 > 0 && !currentBodyPositions.contains(new Coordinate(head.getX() - 1, head.getY()))) {
+            if(head.getX() - 1 >= 0
+                    && !selfOverlap(currentBodyPositions, new Coordinate(head.getX() - 1, head.getY()))) {
                 possibleMoves.add("left");
             }
-            if(head.getX() + 1 <= moveRequest.get("board").get("width").asInt()
-                    && !currentBodyPositions.contains(new Coordinate(head.getX() + 1, head.getY()))) {
+            if(head.getX() + 1 < moveRequest.get("board").get("width").asInt()
+                    && !selfOverlap(currentBodyPositions, new Coordinate(head.getX() + 1, head.getY()))) {
                 possibleMoves.add("right");
             }
-            if(head.getY() - 1 > 0 && !currentBodyPositions.contains(new Coordinate(head.getX(), head.getY() - 1))) {
+            if(head.getY() - 1 >= 0
+                    && !selfOverlap(currentBodyPositions, new Coordinate(head.getX(), head.getY() - 1))) {
                 possibleMoves.add("down");
             }
-            if(head.getY() + 1 <= moveRequest.get("board").get("height").asInt()
-                    && !currentBodyPositions.contains(new Coordinate(head.getX(), head.getY() + 1))) {
+            if(head.getY() + 1 < moveRequest.get("board").get("height").asInt()
+                    && !selfOverlap(currentBodyPositions, new Coordinate(head.getX(), head.getY() + 1))) {
                 possibleMoves.add("up");
             }
 
-//            currentBodyPositions.forEach(coordinate -> {
-//                if(!coordinate.equals(head.getX() - 1, head.getY()) && head.getX() - 1 >= 0){
-//                    possibleMoves.add("left");
-//                } else if(!coordinate.equals(head.getX() + 1, head.getY())
-//                        && head.getX() + 1 <= moveRequest.get("board").get("width").asInt()) {
-//                    possibleMoves.add("right");
-//                } else if(!coordinate.equals(head.getX(), head.getY() - 1) && head.getY() - 1 >= 0) {
-//                    possibleMoves.add("down");
-//                } else if(!coordinate.equals(head.getX(), head.getY() + 1)
-//                        && head.getY() + 1 <= moveRequest.get("board").get("height").asInt()) {
-//                    possibleMoves.add("up");
-//                }
-//            });
-
             return possibleMoves;
+        }
+
+        Boolean selfOverlap(List<Coordinate> bodyPositions, Coordinate coordinate) {
+
+            for(int i = 0; i < bodyPositions.size(); i++) {
+                if(bodyPositions.get(i).equals(coordinate)) {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 
